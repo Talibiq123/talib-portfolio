9938d82ca1e9009f6015fe29efe78f681a76689c
Talibiq123 <talibsaeed90@gmail.com>
2024-12-04 20:23:11 +0530

    services...

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Services.css'
+import theme_pattern from '../../Asssets/theme_pattern.svg'
 
 const Services = () => {
   return (
-    <div>
-      
+    <div className='services'>
+      <div class="services-title">
+        <h1>My Services</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div class="services-container">
+        
+      </div>
     </div>
   )
 }
