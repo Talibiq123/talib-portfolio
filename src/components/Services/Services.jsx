732c90d32ca039f40d44dec388ee08fef11439b3
Talibiq123 <talibsaeed90@gmail.com>
2024-12-06 19:50:27 +0530

    services....

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Services.css'
 import theme_pattern from '../../Asssets/theme_pattern.svg'
+import Services_Data from '../../Asssets/services_data'
+import arrow_icon from '../../Asssets/arrow_icon.svg'
 
 const Services = () => {
   return (
@@ -10,7 +12,17 @@ const Services = () => {
         <img src={theme_pattern} alt="" />
       </div>
       <div class="services-container">
-        
+        {Services_Data.map((service, index) => {
+          return <div key={index} className='service-format'>
+            <h3>{service.s_no}</h3>
+            <h2>{service.s_name}</h2>
+            <p>{service.s_desc}</p>
+            <div className='services.readmore'>
+              <p>read more...</p>
+              <img src={arrow_icon} alt="" />
+            </div>
+          </div>
+        })}
       </div>
     </div>
   )
