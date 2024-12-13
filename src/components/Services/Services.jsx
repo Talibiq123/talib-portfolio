ef589017afae46072018343dcd1b2e3d0d94e38e
Talibiq123 <talibsaeed90@gmail.com>
2024-12-13 20:44:39 +0530

    Services complete....

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -17,7 +17,7 @@ const Services = () => {
             <h3>{service.s_no}</h3>
             <h2>{service.s_name}</h2>
             <p>{service.s_desc}</p>
-            <div className='services.readmore'>
+            <div className='services-readmore'>
               <p>read more...</p>
               <img src={arrow_icon} alt="" />
             </div>
